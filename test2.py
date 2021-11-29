c8af5b95a1041f0f9e92479d34c2ae6923c67a09
Duc Anh <ducanhhbtt@gmail.com>
2021-11-29 15:00:12 +0700

    wrong_commit

---
+++ test2.py
@@ -0,0 +1,31 @@
+from my_lib import Object
+
+import os
+
+from my_lib import Object3
+
+from my_lib import Object2
+
+import sys
+
+from third_party import lib15, lib1, lib2, lib3, lib4, lib5, lib6, lib7, lib8, lib9, lib10, lib11, lib12, lib13, lib14
+
+import sys
+
+from __future__ import absolute_import
+
+from third_party import lib3
+
+print("Hey")
+print("yo")
+
+
+def calculate_something(a,b,c,d,e ,f ,g,h ):
+    i = a+b
+    j = (c+d) /2
+    k =e*f
+    m=  g-h
+    #Check if i is an odd number
+    if(i%2) ==1:
+        i += 1
+    return i + j+k+ m
